df60f35731a62acd3ba33bb5e3556da3be4c5d03
Jake Callcut <jakecallcut123@gmail.com>
2021-05-12 21:36:37 +0100

    made playable started on win conditions

--- Board.cs
+++ Board.cs
@@ -4,8 +4,7 @@ namespace Connect4
 {
     class Board
     {
-            public string[,] boardArray = new string[7,7] {
-                {"- ", "- ", "- ", "- ", "- ", "- ", "- "},
+            public string[,] boardArray = new string[6,7] {
                 {"- ", "- ", "- ", "- ", "- ", "- ", "- "},
                 {"- ", "- ", "- ", "- ", "- ", "- ", "- "},
                 {"- ", "- ", "- ", "- ", "- ", "- ", "- "},
@@ -14,13 +13,100 @@ namespace Connect4
                 {"- ", "- ", "- ", "- ", "- ", "- ", "- "},
             };
 
+        public bool HasWon(string _player)
+        {
+            
+        }
+
+        public bool HasAdjacent(string _player, int _x, int _y, int _direction)  
+        {
+            { 
+            /*
+            _direction values:
+            1 = top left
+            2 = top middle
+            3 = top right
+            4 = middle left
+            5 = middle right
+            6 = bottom left
+            7 = bottom middle
+            8 = bottom right
+            */
+            }
+
+            switch (_direction)
+            {
+                case 1:
+                    if (this.boardArray[_y, _x] == this.boardArray[_y - 1, _x - 1])
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                case 2:
+                    if (this.boardArray[_y, _x] == this.boardArray[_y - 1, _x])
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                case 3:
+                    if (this.boardArray[_y, _x] == this.boardArray[_y - 1, _x + 1])
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                case 4:
+                    if (this.boardArray[_y, _x] == this.boardArray[_y, _x - 1])
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                case 5:
+                    if (this.boardArray[_y, _x] == this.boardArray[_y, _x + 1])
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                case 6:
+                    if (this.boardArray[_y, _x] == this.boardArray[_y + 1, _x - 1])                     //last edit, thinking about so many logic errors
+                    {                                                                                   //what if the vector goes outside of the board?
+                        return true;                                                                    //must be a beter way to do it            
+                    }                                                                                    
+                    else
+                    {
+                        return false;
+                    }
+                case 7:
+
+                case 8:
+
+                default:
+                    return false;
+            }
+            
+        }
+
         public int Add(string _player, int _column)
         {
-            for (int i = 6; i > 0; i--)
+            for (int i = 5; i > 0; i--)
             {
-                if (this.boardArray[i, _column] == "- ")
+                if (this.boardArray[i, _column - 1] == "- ")
                 {
-                    this.boardArray[i, _column] = _player + " ";
+                    this.boardArray[i, _column - 1] = _player + " ";
                     return 1;
                 }
             }
@@ -29,12 +115,12 @@ namespace Connect4
 
         public void Display()
         {
-            for (int j = 0; j < 7; j++)
+            for (int j = 0; j < 6; j++)
             {
                 Console.WriteLine(" ");
                 for (int i = 0; i < 7; i++)
                 { 
-                    Console.Write(this.boardArray[1,i]);
+                    Console.Write(this.boardArray[j,i]);
                 }
             }
             Console.WriteLine(" ");
--- Program.cs
+++ Program.cs
@@ -6,20 +6,32 @@ namespace Connect4
     {
         static void Main(string[] args)
         {
+            bool gameOngoing = true;
+            int turnNumber = 0;
+            string currPlayer = "";
             var board = new Board();
+            
             board.Display();
             Console.ReadKey();
 
-            while (true)
+            while (gameOngoing == true)
             {
-                Console.WriteLine("Crosses, enter a column");
-                int column = Convert.ToInt16(Console.ReadLine());
-                board.Add("X", column);
+                if (turnNumber % 2 == 0)
+                {
+                    currPlayer = "X";
+                }
+                else if (turnNumber % 2 == 1)
+                {
+                    currPlayer = "O";
+                }
+                turnNumber++;
+
+                Console.WriteLine("{0}'s turn, please enter a column", currPlayer);
+                int column = Convert.ToInt32(Console.ReadLine());
+                board.Add(currPlayer, column);
                 Console.Clear();
                 board.Display();
-                Console.ReadKey();
             }
-
         }
     }
 }
